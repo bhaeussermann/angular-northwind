76ffdac073966d78fdbbb7f4cb6e764decba2853
Bernhard Häussermann <bernhardgh@icloud.com>
2021-01-06 09:45:42 +0200

    Add express server for hosting built artifact

---
+++ server.js
@@ -0,0 +1,32 @@
+'use strict';
+const express = require('express');
+const history = require('connect-history-api-fallback');
+const bodyParser = require('body-parser');
+const path = require('path');
+const request = require('request');
+require('dotenv').config();
+
+const app = express();
+app.use(history());
+app.use(bodyParser.json());
+
+app.use('/', express.static(path.join(__dirname, 'dist/northwind')));
+
+app.all('/api/*', function(req, res) {
+  request('https://northwind-express-api.herokuapp.com' + req.url.substring(4), 
+    {
+      method: req.method,
+      body: (req.method === 'POST' || req.method === 'PUT') ? JSON.stringify(req.body) : null,
+      headers: { 'Content-Type': req.headers['content-type'] }
+    },
+    function(error, response, body) {
+      if (error)
+        res.status(500).send({ message: 'Error calling Northwind service: ' + (error.code ? error.code : error) });
+      else
+        res.status(response.statusCode).send(body);
+    });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Northwind web server started on port ' + port);
